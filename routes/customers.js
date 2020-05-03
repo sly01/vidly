49c75926e566aa4f63b0580d56c2f66b839784be
sly01 <aerkoc01@gmail.com>
2020-05-03 06:53:51 +0200

    Customer endpoints added

---
+++ routes/customers.js
@@ -0,0 +1,61 @@
+const { Customer, validate } = require("../models/customers");
+const express = require("express");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  const customers = await Customer.find().sort({ name: 1 });
+  res.send(customers);
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const customer = await Customer.findById(req.params.id);
+    if (!customer) return res.status(404).send("No customer found");
+    res.send(customer);
+  } catch (err) {
+    res.send(err);
+  }
+});
+
+router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.send(error.details[0].message);
+
+  let customer = new Customer({
+    name: req.body.name,
+    phone: req.body.phone,
+    isGold: req.body.isGold,
+  });
+
+  customer = await customer.save();
+  res.send(customer);
+});
+
+router.put("/:id", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.send(error.details[0].message);
+
+  const customer = await Customer.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: {
+        name: req.body.name,
+        isGold: req.body.isGold,
+        phone: req.body.phone,
+      },
+    },
+    { new: true }
+  );
+  if (!customer) return res.status(404).send("No customer found");
+
+  res.send(customer);
+});
+
+router.delete("/:id", async (req, res) => {
+  const customer = await Customer.findByIdAndRemove(req.params.id);
+  if (!customer) return res.status(404).send("No customer found");
+
+  res.send(customer);
+});
+
+module.exports = router;
