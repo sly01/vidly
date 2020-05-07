02ec286cc38b4ae89dd31c539deed6a15babd0e0
sly01 <aerkoc01@gmail.com>
2020-05-07 23:26:52 +0200

    Rental models and routes are added

--- models/movie.js
+++ models/movie.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const { genreSchema } = require("./genre");
 
 const Movie = mongoose.model(
-  "Movies",
+  "Movie",
   new mongoose.Schema({
     title: {
       type: String,
