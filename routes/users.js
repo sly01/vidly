5128a1fee22845ff90ae0e248a620f3b1bef105f
sly01 <aerkoc01@gmail.com>
2020-05-10 20:01:17 +0200

    Lodash added for users

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const router = express.Router();
 const {User, validateUser} = require('../models/user');
@@ -19,18 +20,10 @@ router.post("/", async (req, res) => {
     let user = await User.findOne({ email: req.body.email});
     if (user) return res.status(400).send('User already registered.');
 
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    });
-
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
     await user.save();
-    res.send({
-        name: user.name,
-        email: user.email
-    });
 
+    res.send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router;
