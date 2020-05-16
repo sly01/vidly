bb9cfc23f10872e49b704349c3d147e6778f7c51
sly01 <aerkoc01@gmail.com>
2020-05-16 02:51:03 +0200

    Refactoring for token creation

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
+const config = require('config');
+const jwt = require('jsonwebtoken');
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
-const User = mongoose.model('User', new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
@@ -17,7 +19,13 @@ const User = mongoose.model('User', new mongoose.Schema({
         minlenght: 5,
         maxlength: 1024
     }
-}));
+});
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id}, config.get('jwtPrivateKey'));
+    return token;
+}
+const User = mongoose.model('User', userSchema);
 
 
 function validateUser(user) {
--- routes/users.js
+++ routes/users.js
@@ -29,7 +29,7 @@ router.post("/", async (req, res) => {
 
     await user.save();
 
-    const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
+    const token = user.generateAuthToken();
     res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
 });
 
