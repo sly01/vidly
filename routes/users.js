de2f827cc0b4cd912f62a6b361f788c90ef36665
sly01 <aerkoc01@gmail.com>
2020-05-16 02:43:38 +0200

    Setting response header jwt

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const bcrypt = require('bcrypt');
 const _ = require('lodash');
 const express = require('express');
@@ -27,7 +29,8 @@ router.post("/", async (req, res) => {
 
     await user.save();
 
-    res.send(_.pick(user, ['_id', 'name', 'email']));
+    const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
+    res.header('x-auth-token', token).send(_.pick(user, ['_id', 'name', 'email']));
 });
 
 module.exports = router;
