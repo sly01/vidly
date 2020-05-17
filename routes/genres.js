cb5ba001eca6b6ee7b44ec47befdaa47a04595f1
sly01 <aerkoc01@gmail.com>
2020-05-17 11:05:05 +0200

    Error middleware added

--- routes/genres.js
+++ routes/genres.js
@@ -4,9 +4,13 @@ const { Genre, validateGenre } = require("../models/genre");
 const express = require("express");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const genres = await Genre.find().sort({ name: 1 });
-  res.send(genres);
+router.get("/", async (req, res, next) => {
+  try {
+    const genres = await Genre.find().sort({ name: 1 });
+    res.send(genres);
+  } catch (ex) {
+    next(ex);  
+  }
 });
 
 router.get("/:id", async (req, res) => {
