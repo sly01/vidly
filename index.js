9b6642f1e4f0376af5a54dd469155ebd924365fe
sly01 <aerkoc01@gmail.com>
2020-05-17 12:27:24 +0200

    Separation of concerns for config

--- index.js
+++ index.js
@@ -1,38 +1,13 @@
-require("express-async-errors");
-const winston = require("winston");
-require("winston-mongodb");
-const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const express = require("express");
 
 const app = express();
 
-require("./startup/db")();
+require("./startup/logging")();
 require("./startup/routes")(app);
-
-winston.handleExceptions(
-  new winston.transports.File({ filename: "uncaughtExceptions.log" })
-);
-
-process.on("unhandledRejection", (ex) => {
-  throw ex;
-});
-
-winston.add(winston.transports.File, { filename: "logfile.log" });
-winston.add(winston.transports.MongoDB, {
-  db: "mongodb://localhost/vidly",
-  level: "info",
-});
-
-// throw new Error("Something failed during startup.");
-// const p = Promise.reject(new Error("Something failed miserably!"));
-// p.then(() => console.log("Done"));
-
-if (!config.get("jwtPrivateKey")) {
-  console.error("FATAL ERROR: jwtPrivateKey is not defined.");
-  process.exit(1);
-}
+require("./startup/db")();
+require("./startup/config")();
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
