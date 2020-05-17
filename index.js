d0d581f372e559aeb7eac81351d12ab4ceaea0e5
sly01 <aerkoc01@gmail.com>
2020-05-17 11:58:39 +0200

    Separation of concerns for routes

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 require("express-async-errors");
 const winston = require("winston");
 require("winston-mongodb");
-const error = require("./middleware/error");
 const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
-const logger = require("./logger");
-const helmet = require("helmet");
-const morgan = require("morgan");
 const express = require("express");
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
+
 const app = express();
+
+require("./startup/routes")(app);
 
 winston.handleExceptions(
   new winston.transports.File({ filename: "uncaughtExceptions.log" })
@@ -45,24 +38,6 @@ mongoose
   .connect("mongodb://localhost/vidly")
   .then(() => console.log("Connected to MongoDB..."))
   .catch((err) => console.log("Could not connect to MongoDB...", err.message));
-
-app.use(express.json());
-app.use(helmet());
-
-if (app.get("env") === "development") {
-  app.use(morgan("combined"));
-  console.log("Morgan enabled");
-}
-
-app.use(logger);
-
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ startup/routes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const genres = require("../routes/genres");
+const customers = require("../routes/customers");
+const movies = require("../routes/movies");
+const rentals = require("../routes/rentals");
+const users = require("../routes/users");
+const auth = require("../routes/auth");
+const error = require("../middleware/error");
+
+module.exports = function (app) {
+  if (app.get("env") === "development") {
+    app.use(morgan("combined"));
+    console.log("Morgan enabled");
+  }
+  app.use(express.json());
+  app.use(helmet());
+  app.use("/api/genres", genres);
+  app.use("/api/customers", customers);
+  app.use("/api/movies", movies);
+  app.use("/api/rentals", rentals);
+  app.use("/api/users", users);
+  app.use("/api/auth", auth);
+  app.use(error);
+};
