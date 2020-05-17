4cdb96901a09de831cf011fab9b777a543354e12
sly01 <aerkoc01@gmail.com>
2020-05-17 12:07:24 +0200

    Separation of concerns for db

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ require("winston-mongodb");
 const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
-const mongoose = require("mongoose");
 const express = require("express");
 
 const app = express();
 
+require("./startup/db")();
 require("./startup/routes")(app);
 
 winston.handleExceptions(
@@ -33,11 +33,6 @@ if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR: jwtPrivateKey is not defined.");
   process.exit(1);
 }
-
-mongoose
-  .connect("mongodb://localhost/vidly")
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.log("Could not connect to MongoDB...", err.message));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
